f0c56c6763d6b5a01d8e2251db25abd8316bf7fe
samwisee <mboos@outlook.com>
2018-03-01 13:49:36 +0100

    updated version nr

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'rb') as f:
 setup(
     name='airtiler',
     packages=['airtiler'],
-    version='0.1',
+    version='0.2.0',
     description='The airtiler generates training / test data for neural networks by downloading buildings from vector '
                 'data from OpenStreetMap and the corresponding satellite images from Microsoft Bing Maps.',
     long_description=readme,
