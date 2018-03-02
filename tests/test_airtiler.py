d9c87f815294724e9e4f4e1ab1591a8be8e262a9
samwisee <mboos@outlook.com>
2018-03-02 09:35:52 +0100

    * tests updated * version increased

--- tests/test_airtiler.py
+++ tests/test_airtiler.py
@@ -1,22 +1,45 @@
 from airtiler import Airtiler
 import json
 import os
+import glob
 
 empty_config = """
 {
   "options": {
     "target_dir": "./output",
-    "zoom_levels": [18, 19],
+    "zoom_levels": [18],
     "separate_instances": false
   },
   "boundingboxes": {
-    "empty": [11, 43, 10, 43],
+    "empty": [11, 43, 10, 43]
+  }
+}
+"""
+
+single_building_config = """
+{
+  "options": {
+    "target_dir": "./output",
+    "zoom_levels": [18],
+    "separate_instances": false
+  },
+  "boundingboxes": {
     "single_building": [8.8183594613,47.2228679539,8.819253978,47.2234162581]
   }
 }
 """
 
 
-def test_airtiler():
+def test_empty_config():
     config = json.loads(empty_config)
-    Airtiler(os.environ.get("BING_KEY", "")).process(config)
+    Airtiler("").process(config)
+
+
+def test_single_building_config():
+    config = json.loads(single_building_config)
+    key = os.environ.get("BING_KEY", "")
+    Airtiler(key).process(config)
+    images = glob.glob("./output/**/*.tif", recursive=True)
+    expected_nr_images = 1 if not key else 2  # on travis the bing key is set and therefore the tile can be downloaded
+    assert len(images) == expected_nr_images
+
